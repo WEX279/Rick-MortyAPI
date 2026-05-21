6bc7fbfa2c284cb29a583468ac92aee0ce393fce
WEX_279 <legazyuri279@gmail.com>
2026-05-21 20:00:53 +0200

    feat: fix app.js

--- app.js
+++ app.js
@@ -1,11 +1,80 @@
-const api = fetch('https://rickandmortyapi.com/api/')
+const url = 'https://rickandmortyapi.com/api'
 
-const buscador = document.getElementById(buscador);
+const input = document.getElementById("input")
+const button = document.getElementById("button");
 
-const button = document.getElementById(button);
-button.addEventListener ("click")
-button.addEventListener("keyDown", (event) => {
-    if (event.key === 'enter'){
-        console.log("va bien")
-    }
+
+button.addEventListener("click", (eventClick) => {
+    console.log(input.value);
+    input.value = "";
 })
+
+
+async function getAllCharacters() {
+    try {
+        const url = 'https://rickandmortyapi.com/api';
+        const res = await fetch(`${url}/character`);
+        const data = await res.json();
+        renderCharacters(data.results)
+        console.log(data);
+        return data;
+
+    } catch (error){
+        console.error("Fallo del sistema", error);
+        return null;
+    }
+}
+      
+function charIcons(charList){
+    const getIcons = charList.filter((image) => image.map = "image")
+    console.log(getIcons)
+}
+
+function renderCharacters(charList){
+    const getAliveCharacters = charList.filter((character) => character.status === "Alive")
+console.log(getAliveCharacters)
+
+} 
+
+getAllCharacters()
+
+
+function card(){
+
+    const container = document.querySelector("#container")
+    container.innerHTML = `
+    <div class="card">
+            <div>
+                <img class="charPic" src=${image}>
+            </div>
+            <div>
+                <div>
+                    <div>
+                    
+                    </div>
+                    <div>
+                    
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <p>ejemplo</p>
+                    </div>
+                    <div>
+                    
+                    </div>
+                </div>
+                <div>
+                    <div>
+                    
+                    </div>
+                    <div>
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+}
+
+card()
