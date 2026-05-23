8d706b3df4f2f3c7cd805e0ddd3ab2c68ce301ed
WEX_279 <legazyuri279@gmail.com>
2026-05-23 23:56:08 +0200

    feat: add interactivity and responsive

--- app.js
+++ app.js
@@ -2,21 +2,97 @@ const url = 'https://rickandmortyapi.com/api'
 
 const input = document.getElementById("input")
 const button = document.getElementById("button");
+const container = document.querySelector("#container");
+
+let nextPage = null;
+let prevPage = null;
+
+let currentPage = `${url}/character`;
 
 
-button.addEventListener("click", async (eventClick) => {
-    console.log(input.value);
-    input.value = "";
+async function fetchData() {
+    
+    try{
+        const input = document.getElementById("input");
+        const value = input.value.toLowerCase().trim();
+
+        const res = await fetch(`https://rickandmortyapi.com/api/character/?name=${value}`);
+
+    if(!res.ok){
+        throw new Error("Could not fetch resource");
+    }
+    
+    const data = await res.json();
+
+    
+
+    container.innerHTML = "";
+
+    data.results.forEach((character) => {
+        
+        container.innerHTML +=`
+        
+        
+    <div class="card">
+            <div>
+                <img class="charPic" src="${character.image}">
+            </div>
+            <div class="cardProfile">
+                <div class="charInfo">
+                    <div class="charStatus">
+                        <h2 class="textStatus">${character.name}</h2>
+                        <h3 class="textStatus">${character.status} - ${character.species}</h3>
+                    </div>
+                    <div class="charLocation">
+                        <p class="textLocation">Last seen: </p>
+                        <h4 class="textLocation">${character.origin.name}</h4>
+                    </div>
+                </div>
+                <div class="charEpi">
+                    <div>
+                        <p class="textEpi">First seen:</p>
+                        <h4 class="textEpi">  ${character.episode[0]}</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
 })
+    }catch(error) {
+        console.error(error)
+    }
+    input.value = "";
+
+        data.results.forEach(character => {
+        console.log(character.name);
+        }); 
+}
+
+
+let page = url;
+
+    
+button.addEventListener("click", (eventClick) => {
+    fetchData()    
+});
+
+input.addEventListener("keydown", (event) => {
+    if(event.key === "Enter"){
+    fetchData()    
+    }
+});
 
 
 async function getAllCharacters() {
     try {
         const url = 'https://rickandmortyapi.com/api';
+        
+        let allCharacter = []
+        let nextPage = `${url}/character`;
+        
         const res = await fetch(`${url}/character`);
         const data = await res.json();
-        // getAliveCharacters(data.results)
-        // charIcons(data.results);
         console.log(data.results);
         return data;
 
@@ -26,10 +102,63 @@ async function getAllCharacters() {
     }
 }
 
-async function card() {
-    const data = await getAllCharacters();
+async function getAllLocations() {
+    try {
+        const url = 'https://rickandmortyapi.com/api';
+        const res = await fetch(`${url}/location`);
+        const data = await res.json();
+        console.log(data.results);
+        return data;
+
+    } catch (error){
+        console.error("Fallo del sistema", error);
+        return null;
+    }
+}      
+
+async function getAllChapters() {
+    try {
+        const url = 'https://rickandmortyapi.com/api';
+        const res = await fetch(`${url}/episode`);
+        const data = await res.json();
+        console.log(data.results);
+        return data;
+
+    } catch (error){
+        console.error("Fallo del sistema", error);
+        return null;
+    }
+}
+
+async function cardChap() {
+    const data = await getAllChapters();
+    data.results.forEach((episode) => {
+
+    }) 
+}
+
+
+async function loadPage(url) {
+    
+    try{
+        const res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error("couldsnt fetch rsc")
+        }
+
+        const data = await res.json()
+
+        nextPage = data.info.next;
+        prevPage = data.info.prev;
+        currentPage = url;
+
+        container.innerHTML = "";
+    // const char = await getAllCharacters();
+    // char.results.forEach((character) => {
+    // const container = document.querySelector("#container");
     data.results.forEach((character) => {
-    const container = document.querySelector("#container");
+
     container.innerHTML += `
     <div class="card">
             <div>
@@ -38,73 +167,67 @@ async function card() {
             <div class="cardProfile">
                 <div class="charInfo">
                     <div class="charStatus">
-                        <h2 class="charText">${character.name}</h2>
-                        <h3 class="charText">Status: ${character.status} - ${character.species}</h3>
+                        <h2 class="textStatus">${character.name}</h2>
+                        <h3 class="textStatus">${character.status} - ${character.species}</h3>
                     </div>
-                    <div>
-                    
-                    </div>
-                </div>
-                <div>
-                    <div>
-                        <p></p>
-                    </div>
-                    <div>
-                    
+                    <div class="charLocation">
+                        <p class="textLocation">Last seen: </p>
+                        <h4 class="textLocation">${character.origin.name}</h4>
                     </div>
                 </div>
-                <div>
+                <div class="charEpi">
                     <div>
-                    
-                    </div>
-                    <div>
-                    
+                        <p class="textEpi">First seen:</p>
+                        <h4 class="textEpi">  ${character}</h4>
                     </div>
                 </div>
             </div>
         </div>
-    `
-}
-    )};
-
-    card()
-
-// async function getAllLocations() {
-//     try {
-//         const url = 'https://rickandmortyapi.com/api';
-//         const res = await fetch(`${url}/location`);
-//         const data = await res.json();
-//         console.log(data.results);
-//         return data;
-
-//     } catch (error){
-//         console.error("Fallo del sistema", error);
-//         return null;
-//     }
-// }
-
-// getAllLocations()
-      
-// async function getIcons() {
+    </div>
+            `;
+    })
+    }  catch(error) {
+        console.error(error);
     
-//         const res = await getAllCharacters();
-//         const data = await charIcons(res.results);
-//         console.log(data);
-//     };
+    }
+};
 
-// getIcons();
+    loadPage(url)
 
-// function charIcons(charList){
-//     return charList.map((character) => character.image);
-//     console.log(charIcons);
-// };
 
-// charIcons()
+async function charIcons(charList){
+    try{
+    const url ='https://rickandmortyapi.com/api';
+    const res = await fetch(`${url}/character`);
+    const data = await res.json();
+    const charIMG = await data.results.map((character) => character.image);
+    console.log(charIMG);
+    } catch(error){
+        console.error("Fallo del sistema", error);
+        return null;
+    }
+};
 
-// function charStatus(charList){
-//     return charList.map((character) => character.status);
-//     console.log(charStatus);
-// };
+charIcons()
+
+function charStatus(charList){
+    return charList.map((character) => character.status);
+    console.log(charStatus);
+};
+
+document.getElementById("next").addEventListener("click", () =>{
+    if(nextPage){
+        loadPage(nextPage);
+    }
+})
+
+document.getElementById("prev").addEventListener("click", () =>{
+    if(prevPage){
+        loadPage(prevPage);
+    }
+})
+
+loadPage(currentPage);
 
 // function getAliveCharacters(charList){
 //     const getAliveCharacters = charList.filter((character) => character.status === "Alive")
@@ -113,9 +236,3 @@ async function card() {
 
 // getAliveCharacters()
 
-
-// function card(){
-
-    
-
-// card()
